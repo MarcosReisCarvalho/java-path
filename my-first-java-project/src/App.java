64f0e682f7adb8e55943e964b4f0c68b741cab71
Marcos Reis <marcosreis09122000@gmail.com>
2024-09-04 15:28:42 -0300

    receiving inputs

--- my-first-java-project/src/App.java
+++ my-first-java-project/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Digite seu primeiro nome e segundo nome abaixo do primeiro: ");
+
+        //String Input
+        String name = myObj.nextLine();
+        String name2 = myObj.nextLine();
+
+        System.out.print(nome(name, name2));
+
     }
+
+    public static String nome(String primerioNome, String segundoNome) {
+        return "metodo retorno: " + primerioNome.concat(" ").concat(segundoNome);
+    };
 }
