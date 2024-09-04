7e2038dfab6fe6503d3344e44b3daa5a9fa0cf06
Marcos Reis <marcosreis09122000@gmail.com>
2024-09-04 17:28:59 -0300

    fixing app class

--- my-first-java-project/src/App.java
+++ my-first-java-project/src/App.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Digite seu primeiro nome e segundo nome abaixo do primeiro: ");
-
-        //String Input
-        String name = myObj.nextLine();
-        String name2 = myObj.nextLine();
-
-        System.out.print(nome(name, name2));
-
-        myObj.close();
+        try (Scanner myObj = new Scanner(System.in)) {
+            System.out.println("Digite seu primeiro nome e segundo nome abaixo do primeiro: ");
+            
+            //String Input
+            String name = myObj.nextLine();
+            String name2 = myObj.nextLine();
+            
+            System.out.print(nome(name, name2));
+        }
     }
 
     public static String nome(String primerioNome, String segundoNome) {
