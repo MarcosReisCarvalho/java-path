facb8387b2cf0e32d99588b2e40c24faae7064fd
Marcos Reis <marcosreis09122000@gmail.com>
2024-09-06 10:52:00 -0300

    Definindo atributos

---
+++ my-first-java-project/src/edu/marcos/primeirasemana/segundasemana/SmartTv.java
@@ -0,0 +1,11 @@
+package edu.marcos.primeirasemana.segundasemana;
+
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    
+    public static void main(String args){
+
+    }
+}
