d8ee9b90cb958379e19d53ad3629ee32c92c9307
Marcos Reis <marcosreis09122000@gmail.com>
2024-09-10 11:47:05 -0300

    using Break in for loop

---
+++ my-first-java-project/src/edu/marcos/primeirasemana/estruturasrepeticao/ExemploBreakContinue.java
@@ -0,0 +1,11 @@
+package edu.marcos.primeirasemana.estruturasrepeticao;
+
+public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for(int contador = 1; contador <= 5; contador++) {
+            if (contador == 3)
+                break;
+            System.out.println(contador);
+        }
+    }
+}
