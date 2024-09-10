8447edc066c4e2cebb9a62cc4b44516edaf567ac
Marcos Reis <marcosreis09122000@gmail.com>
2024-09-10 12:04:03 -0300

    using while

--- my-first-java-project/src/edu/marcos/primeirasemana/estruturasrepeticao/ExemploBreakContinue.java
+++ my-first-java-project/src/edu/marcos/primeirasemana/estruturasrepeticao/ExemploBreakContinue.java
@@ -4,7 +4,7 @@ public class ExemploBreakContinue {
     public static void main(String[] args) {
         for(int contador = 1; contador <= 5; contador++) {
             if (contador == 3)
-                continue;
+                continue; // Não para a aplicação, mas mudou o fluxo, o número 3 não foi impresso no console.
             System.out.println(contador);
         }
     }
