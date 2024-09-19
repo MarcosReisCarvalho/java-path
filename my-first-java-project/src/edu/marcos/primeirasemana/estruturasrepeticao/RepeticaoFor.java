a894b36f5594f33b5644679d03d9c91fccda2452
Marcos Reis <marcosreis09122000@gmail.com>
2024-09-19 08:22:27 -0300

    testes for

---
+++ my-first-java-project/src/edu/marcos/primeirasemana/estruturasrepeticao/RepeticaoFor.java
@@ -0,0 +1,21 @@
+package edu.marcos.primeirasemana.estruturasrepeticao;
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Problem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+// TODO: complete os espaços em branco com sua solução para o problema
+        String[] nomes = new String[     ];
+
+        for(int i=0 ; i<10 ; i++)
+            nomes[    ] = sc.nextLine();
+
+
+        System.out.println(nomes[3]);
+        System.out.println(nomes[7]);
+        System.out.println(nomes[9]);
+    }
+}
